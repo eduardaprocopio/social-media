e28d9fd5721a02f7716198c2631ce0b338348647
Eduarda Procópio <eduardaprocopio14@gmail.com>
2022-07-26 11:25:52 -0300

    finalização da aplicação com a criação das funções de editar e deletar

--- main.js
+++ main.js
@@ -26,8 +26,28 @@ let data = {};
 let acceptData = () => {
   data["text"] = input.value;
   console.log(data);
+  
+  createPost();
 };
 
-let creatPost = () => {
-  posts.innerHTML += ``;
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
 };
